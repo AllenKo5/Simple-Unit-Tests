6b6a81eb2ea09f6d09e5ba901f4d18e8a268f365
Allen Ko (RIT Student) <ask9458@rit.edu>
2022-11-28 15:42:50 -0500

    set up unit tests

--- server/test/utils/index.js
+++ server/test/utils/index.js
@@ -1,5 +1,46 @@
-// const config = require('../test_config.js');
-// const utils = require('../../src/utils');
-//
-// const { limits } = config;
-//
+const config = require('../test_config.js');
+const utils = require('../../src/utils');
+
+const { limits } = config;
+
+describe('utils', () => {
+  describe('add', () => {
+    it('should return addition of two numbers', (done) => {
+      const sum = utils.add(5, 10);
+      sum.should.equal(15);
+      done();
+    });
+
+    it('should clamp number to minimum', (done) => {
+      const sum = utils.add(limits.MIN - 10, 5);
+      sum.should.equal(limits.MIN);
+      done();
+    });
+
+    it('should clamp number to maximum', (done) => {
+      const sum = utils.add(limits.MAX, 5);
+      sum.should.equal(limits.MAX);
+      done();
+    });
+  });
+
+  describe('subtract', () => {
+    it('should return difference of two numbers', (done) => {
+      const diff = utils.sub(10, 5);
+      diff.should.equal(5);
+      done();
+    });
+
+    it('should clamp number to minimum', (done) => {
+      const diff = utils.sub(limits.MIN, 5);
+      diff.should.equal(limits.MIN);
+      done();
+    });
+
+    it('should clamp number to maximum', (done) => {
+      const diff = utils.sub(limits.MAX + 10, 5);
+      diff.should.equal(limits.MAX);
+      done();
+    });
+  });
+});
